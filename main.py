8d755af3b79a995b5f131b1599bd425c1877954c
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-07-18 20:48:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import asyncoo 
+import subprocess 
+import json
 import os 
-import 
+import shutil
+from datetime import datetime
+from pathlib import Path
+import paramiko
+from mcp import server, types 
+
+app = Server("Linux-config-server")
+
+
+
+TARGET_SYSTEM  = {
+    "localhost":{
+        "host": "localhost",
+        "type": "local"
+    }
+
+# add in the servers
+# add path to what dir you want to acces 
